569ddbd5df1d95393bf1d848d65c6f9360cf7288
khayliang <khayliang@gmail.com>
2022-10-15 15:38:43 +0800

    configure eslint for jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
     browser: true,
     commonjs: true,
     es2021: true,
+    'jest/globals': true,
+
   },
+  plugins: ['jest'],
   extends: 'eslint-config-airbnb-base',
   overrides: [
   ],
@@ -11,5 +14,10 @@ module.exports = {
     ecmaVersion: 'latest',
   },
   rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
 };
