a03b06adda26fed7a4fe7b03f7a6318746394ebf
khayliang <khayliang@gmail.com>
2022-10-28 14:18:57 +0800

    rename api from createNewVehicle to registerVehicle

--- api/registerVehicle.js
+++ api/registerVehicle.js
@@ -2,9 +2,9 @@ const { default: axios } = require('axios');
 const snakeize = require('snakeize');
 const { api } = require('./api');
 
-const addr = api.createNewVehicle;
+const addr = api.registerVehicle;
 
-module.exports.createNewVehicle = async (vehicleData) => {
+module.exports.registerVehicle = async (vehicleData) => {
   const resp = await axios.post(addr, snakeize(vehicleData));
   return resp;
 };
