b10e379fedc680f18fb13b999bf97205ffbb0608
khayliang <khayliang@gmail.com>
2022-10-30 16:45:10 +0800

    linting

--- routes/feedbackRoute.js
+++ routes/feedbackRoute.js
@@ -3,21 +3,20 @@ const {
 } = require('../interactions/formInteractionTemplate');
 const { buildButtonFunction } = require('../utils/buildButtonFunction');
 
-
 const feedbackForm = {
   entries: [
     {
       key: 'problem',
       title: 'Got a problem?',
       type: 'buttons',
-      buttons: buildButtonFunction(Object.entries({yes: "Yes", no: "No"})),
-      verify: ({data}) => {
-        if (data === "no") return false
-        return true
+      buttons: buildButtonFunction(Object.entries({ yes: 'Yes', no: 'No' })),
+      verify: ({ data }) => {
+        if (data === 'no') return false;
+        return true;
       },
       prompt: () => 'You got problem with me is it?',
-      success: ({ data }) => `Walao I do so much still want to complain.`,
-      error: ({ data }) => `Oi don't shy eh got problem then say lah`,
+      success: () => 'Walao I do so much still want to complain.',
+      error: () => 'Oi don\'t shy eh got problem then say lah',
     },
     {
       key: 'feedback',
@@ -25,7 +24,7 @@ const feedbackForm = {
       type: 'string',
       verify: () => true,
       prompt: () => 'Got problem then say lah. Faster eh, Don\'t waste my time.',
-      success: () => `Noted with thanks`,
+      success: () => 'Noted with thanks',
       error: () => 'That doesn\'t seem like a valid car plate. Please enter a valid number e.g 33716',
     },
   ],
