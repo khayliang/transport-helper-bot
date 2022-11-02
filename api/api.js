605540523b313b3864b1416a930c7a3fb6a597fa
khayliang <40556327+khayliang@users.noreply.github.com>
2022-11-02 11:14:58 +0800

    add get_vehicle api url

--- api/api.js
+++ api/api.js
@@ -12,4 +12,5 @@ module.exports.api = {
   getUserActivity: `${url}/get_user_activity_of_month`,
   getUsersInUnit: `${url}/get_users_in_army_unit`,
   getWptListForNode: `${url}/get_wpt_list_for_node`,
+  getVehicle: `${url}/get_vehicle`
 };
