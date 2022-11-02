0300f157c88393b82d79135ef705b97ccd7cd335
khayliang <40556327+khayliang@users.noreply.github.com>
2022-11-02 11:25:12 +0800

    added route for getVehicle

---
+++ routes/getVehicleRoute.js
@@ -0,0 +1,39 @@
+const _ = require('lodash');
+const moment = require('moment');
+
+const { getVehicle } = require('../api/getVehicle');
+
+const {
+  formInteractionTemplate,
+} = require('../interactions/formInteractionTemplate');
+
+const getVehicleForm = {
+  entries: [
+    {
+      key: 'vehicle_no',
+      title: 'Vehicle no',
+      type: 'number',
+      verify: ({ data }) => {
+        if `${data}`.length === 5 return true;
+        return false;
+      },
+      prompt: () => 'What vehicle do you want to view?',
+      success: ({ data }) => `You want to view vehicle ${data}`,
+      error: () => 'That doesn\'t seem like a valid number. Please enter a valid vehicle number',
+    },
+  ],
+  verifyPrompt: () => "Select an entry if you want to edit, or submit if you're ready.",
+  onFinish: async (ctx, responses) => {
+    try {
+      const vehicleData = await getVehicle({
+        ...responses,
+      });
+      await ctx.reply(`Raw vehicle data: \n\n${JSON.stringify(vehicleData)}`);
+    } catch (err) {
+      await ctx.reply(`Oops, something went wrong. ${err.message}`);
+    }
+  },
+};
+module.exports.viewMyActivitiesRoute = async (ctx) => {
+  formInteractionTemplate(ctx, viewMyActivitiesForm);
+};
