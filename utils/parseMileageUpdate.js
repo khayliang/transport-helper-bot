2f47581ea7684d6408931a3d9423471311a99d58
khayliang <khayliang@gmail.com>
2022-11-20 14:19:13 +0800

    parser fn to process update msg to activity obj

---
+++ utils/parseMileageUpdate.js
@@ -0,0 +1,85 @@
+const moment = require('moment');
+
+const parseValues = [
+  {
+    key: 'purpose',
+  },
+  {
+    key: 'vehicleNo',
+    parse: (val) => {
+      if (Number.isNaN(val)) throw Error('invalid vehicle no');
+      else return parseInt(val, 10);
+    },
+  },
+  {
+    key: 'ivcWorking',
+    parse: (val) => (!!['yes', 'y'].includes(val)),
+  },
+  {
+    key: 'initialDestination',
+  },
+  {
+    key: 'initialTimestamp',
+    parse: (val) => {
+      const dateTime = val.split(' ').filter((el) => el != null);
+      const time = moment(dateTime[0], 'DDMMYY/HHmm', true);
+      if (time.isValid()) return time.unix();
+      throw Error('Invalid starting time format');
+    },
+  },
+  {
+    key: 'initialMileage',
+    parse: (val) => {
+      if (Number.isNaN(val)) throw Error('invalid starting odometer');
+      else return parseInt(val, 10);
+    },
+  },
+  {
+    key: 'finalDestination',
+  },
+  {
+    key: 'finalTimestamp',
+    parse: (val) => {
+      const dateTime = val.split(' ').filter((el) => el != null);
+      const time = moment(dateTime[0], 'DDMMYY/HHmm', true);
+      if (time.isValid()) return time.unix();
+      throw Error('Invalid starting time format');
+    },
+  },
+  {
+    key: 'finalMileage',
+    parse: (val) => {
+      if (Number.isNaN(val)) throw Error('invalid end odometer');
+      else return parseInt(val, 10);
+    },
+  },
+  {
+    key: 'polAmt',
+    parse: (val) => {
+      if (val === 'nil') return 0;
+      if (Number.isNaN(val)) throw Error('invalid pol amount');
+      else return parseInt(val, 10);
+    },
+  },
+  {
+    key: 'polOdo',
+    parse: (val) => {
+      if (val === 'nil') return 0;
+      if (Number.isNaN(val)) throw Error('invalid pol odometer');
+      else return parseInt(val, 10);
+    },
+  },
+];
+
+module.exports.parseMileageUpdate = (msg) => {
+  const valuesArr = msg.toLowerCase().split(/\r?\n/).map(((str) => str.split(': ')[1])).slice(2);
+  // console.log(valuesArr)
+  const activityObj = {};
+  valuesArr.forEach((val, idx) => {
+    const { key, parse = (x) => x } = parseValues[idx];
+    activityObj[key] = parse(val);
+  });
+  // console.log(activityObj)
+
+  return activityObj;
+};
---
+++ utils/parseMileageUpdate.test.js
@@ -0,0 +1,134 @@
+const { parseMileageUpdate } = require('./parseMileageUpdate');
+
+const mileageUpdateText = `Rank / Name : 3SG BRYAN
+Mask NRIC: TXXXX012A
+Purpose: SHIFT VEHICLE
+Vehicle No: 12345
+IVC working: Yes
+Starting destination: Bedok Camp
+Date/Time start: 181122/0415
+Start Odo: 38302
+End destination: Bedok Camp
+Date/Time end: 181122/1635
+End Odo: 38305
+Pol Amt: nil
+Pol Odo: nil`;
+
+test('function parses message text into activity object', async () => {
+  const parsedMsg = parseMileageUpdate(mileageUpdateText);
+  expect(parsedMsg).toEqual({
+    purpose: 'shift vehicle',
+    vehicleNo: 12345,
+    ivcWorking: true,
+    initialDestination: 'bedok camp',
+    finalDestination: 'bedok camp',
+    initialTimestamp: 1668716100,
+    finalTimestamp: 1668760500,
+    initialMileage: 38302,
+    finalMileage: 38305,
+    polAmt: 0,
+    polOdo: 0,
+  });
+});
+
+const mileageUpdateTextNoIvc = `Rank / Name : 3SG BRYAN
+Mask NRIC: TXXXX012A
+Purpose: SHIFT VEHICLE
+Vehicle No: 12345
+IVC working: no
+Starting destination: Bedok Camp
+Date/Time start: 181122/0415
+Start Odo: 38302
+End destination: Bedok Camp
+Date/Time end: 181122/1635
+End Odo: 38305
+Pol Amt: nil
+Pol Odo: nil`;
+
+test('ivc value works', async () => {
+  const parsedMsg = parseMileageUpdate(mileageUpdateTextNoIvc);
+  expect(parsedMsg).toEqual({
+    purpose: 'shift vehicle',
+    vehicleNo: 12345,
+    ivcWorking: false,
+    initialDestination: 'bedok camp',
+    finalDestination: 'bedok camp',
+    initialTimestamp: 1668716100,
+    finalTimestamp: 1668760500,
+    initialMileage: 38302,
+    finalMileage: 38305,
+    polAmt: 0,
+    polOdo: 0,
+  });
+});
+
+const mileageUpdateTextInvalidTime = `Rank / Name : 3SG BRYAN
+Mask NRIC: TXXXX012A
+Purpose: SHIFT VEHICLE
+Vehicle No: 12345
+IVC working: no
+Starting destination: Bedok Camp
+Date/Time start: 181122 1635
+Start Odo: 38302
+End destination: Bedok Camp
+Date/Time end: 181122/1635
+End Odo: 38305
+Pol Amt: nil
+Pol Odo: nil`;
+
+test('invalid time format throws error', async () => {
+  expect(() => {
+    parseMileageUpdate(mileageUpdateTextInvalidTime);
+  }).toThrow();
+});
+
+const invalidVehicleNo = `Rank / Name : 3SG BRYAN
+Mask NRIC: TXXXX012A
+Purpose: SHIFT VEHICLE
+Vehicle No: 1a344
+IVC working: no
+Starting destination: Bedok Camp
+Date/Time start: 181122/1635
+Start Odo: 38302
+End destination: Bedok Camp
+Date/Time end: 181122/1635
+End Odo: 38305
+Pol Amt: nil
+Pol Odo: nil`;
+
+test('invalid vehicle number throws error', async () => {
+  expect(() => {
+    parseMileageUpdate(invalidVehicleNo);
+  }).toThrow();
+});
+
+const polUpdate = `Rank / Name : 3SG BRYAN
+Mask NRIC: TXXXX012A
+Purpose: SHIFT VEHICLE
+Vehicle No: 12345
+IVC working: no
+Starting destination: Bedok Camp
+Date/Time start: 181122/0415
+Start Odo: 38302
+End destination: Bedok Camp
+Date/Time end: 181122/1635
+End Odo: 38305
+Pol Amt: 1233
+Pol Odo: 23333`;
+
+test('POL works', async () => {
+  const parsedMsg = parseMileageUpdate(polUpdate);
+  expect(parsedMsg).toEqual({
+    purpose: 'shift vehicle',
+    vehicleNo: 12345,
+    ivcWorking: false,
+    initialDestination: 'bedok camp',
+    finalDestination: 'bedok camp',
+    initialTimestamp: 1668716100,
+    finalTimestamp: 1668760500,
+    initialMileage: 38302,
+    finalMileage: 38305,
+    polAmt: 1233,
+    polOdo: 23333,
+  });
+});
